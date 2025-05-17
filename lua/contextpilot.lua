1d548d6c3238dd711f5e4c0043296f4291423f60
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2025-05-17 15:12:18 +0530

    Remove the tick mark forever

--- lua/contextpilot.lua
+++ lua/contextpilot.lua
@@ -478,8 +478,7 @@ function A.start_indexing_subdirectory()
             value = entry,
             ordinal = entry,
             display = function(_, selected)
-              local tick = selected and "✓ " or "  "
-              return tick .. entry
+              return entry
             end,
           }
         end,
